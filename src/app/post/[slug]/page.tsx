177b5eb5aec9e2cc76a266f24a928b660025dfb5
ziwonii24 <ziwonii25@gmail.com>
2025-07-30 21:27:16 +0900

    :lipstick: ADD markdown style

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -28,12 +28,18 @@ export default async function Page({
   }
 
   return (
-    <section>
-      <h1>{post.metadata.title}</h1>
-      <p>{post.metadata.publishedAt}</p>
-      <article>
-        <CustomMDX source={post.content} />
-      </article>
+    <section className="pt-12 pb-12">
+      <div className="bg-white w-3xl mt-0 mb-0 ml-auto mr-auto p-8 flex flex-col gap-12 rounded-2xl shadow-2xl">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-4xl font-bold">{post.metadata.title}</h1>
+          <span className="text-sm font-light">
+            {post.metadata.publishedAt}
+          </span>
+        </div>
+        <article className="custom-mdx">
+          <CustomMDX source={post.content} />
+        </article>
+      </div>
     </section>
   );
 }
