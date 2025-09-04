616feb555500dbf77748de83443d3088edcbda46
ziwonii24 <ziwonii25@gmail.com>
2025-09-04 21:49:24 +0900

    :wrench: UPDATE next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  // basePath: "/azalog",
-  // output: "export",
-  // assetPrefix:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://ziwonii24.github.io/azalog"
-  //     : undefined,
+  basePath: "/azalog",
+  output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://ziwonii24.github.io/azalog"
+      : undefined,
   webpack(config) {
     // 기존 svg 처리 로더 제거
     const fileLoaderRule = config.module.rules.find((rule: any) =>
